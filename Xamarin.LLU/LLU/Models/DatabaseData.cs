dea61a8febe6b3d122535a6b7392c5d8cda944a6
NiksSkersts <91e01896ced492fccf9f8131a691c354736a4216>
2021-12-26 21:23:33 +0200

    Update to DatabaseData.cs

--- Xamarin.LLU/LLU/Models/DatabaseData.cs
+++ Xamarin.LLU/LLU/Models/DatabaseData.cs
@@ -1,23 +1,67 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using SQLite;
-using SQLiteNetExtensions.Attributes;
 
 namespace LLU.Models;
 
 public class DatabaseData {
     [PrimaryKey] public string Id { get; set; }
-
-    [ForeignKey(typeof(UserData))] public string Username { get; set; }
+    public string UniqueId { get; set; }
+    public string Folder { get; set; }
     public string From { get; set; }
     public string To { get; set; }
     public string Subject { get; set; }
-    public DateTimeOffset Time { get; set; }
+    public long Time { get; set; }
     public string Body { get; set; }
     public bool IsHtmlBody { get; set; }
     public bool DeleteFlag { get; set; }
 }
 
 public class UserData {
-    [PrimaryKey] [OneToMany] public string Username { get; set; }
+    public string Username { get; set; }
     public string Password { get; set; }
+}
+
+internal class MailStorageSystem {
+    public List<Year> Years;
+
+    public MailStorageSystem(List<Year> years) => Years = years;
+    public Year? SearchYear(int year) => Years.FirstOrDefault(y => year == y.Value);
+    public Month? SearchMonth(int month, Year year) => year.Months.FirstOrDefault(m => m.Value.Equals(month));
+    public List<DatabaseData>? ReturnMail(int year, int month) {
+        var findYear = SearchYear(year);
+        if (findYear is not null) {
+            var findMonth = SearchMonth(month, findYear);
+            if (findMonth is not null)
+                return findMonth.Mail;
+        }
+        return null;
+    }
+    public void SortDatabase() {
+        var newList = Years.OrderBy(predicate=>predicate.Value).ToList();
+        Years = newList;
+    }
+}
+
+class Year {
+    public readonly int Value;
+    public List<Month> Months;
+    public Year(int value,List<Month> months) {
+        Value = value;
+        Months = months;
+    }
+}
+
+class Month {
+    public Year Year;
+    public int Value;
+    public List<DatabaseData> Mail;
+    public int MailCount;
+
+    public Month(Year year,int month,List<DatabaseData> mail) {
+        Value = month;
+        Year = year;
+        Mail = mail;
+        MailCount = mail.Count;
+    }
 }
